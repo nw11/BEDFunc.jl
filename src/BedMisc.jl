616a3162b122e8f2071ee2d4fc3714a0ee70fa51
nw11 <nw11@sanger.ac.uk>
2015-10-25 21:27:50 +0000

    initial commit

--- src/BedMisc.jl
+++ src/BedMisc.jl
@@ -1,5 +1,7 @@
 module BedMisc
 
-# package code goes here
+export bedtools_intersect
+export convert_methpipebed_to_bedfile
+include(Pkg.dir("BedMisc","src","bedtools_intersect.jl"))
 
 end # module
