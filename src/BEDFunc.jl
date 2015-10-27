7444667a89998611eac998729a9651358c843869
nw11 <nw11@sanger.ac.uk>
2015-10-27 18:32:15 +0000

    add load_dataframe_from_url function

--- src/BEDFunc.jl
+++ src/BEDFunc.jl
@@ -4,4 +4,6 @@ export bedtools_intersect
 export convert_methpipebed_to_bedfile
 include(Pkg.dir("BEDFunc","src","bedtools_intersect.jl"))
 
+export load_dataframe_from_url
+include(Pkg.dir("BEDFunc","src","misc.jl"))
 end # module
