f17024b53f9eec455c262f714fef845d8ccd3665
nw11 <nw11@sanger.ac.uk>
2015-12-11 15:44:07 +0000

    actually commit the changes to write_bedfile with the sorting options

--- src/bedtools_intersect.jl
+++ src/bedtools_intersect.jl
@@ -3,21 +3,32 @@ using DataFrames
 # when dataframes get written, by default they output
 # comma delimited with a header, and strings are quoted.
 # these things need to be rectified.
-function write_bedfile(output_path,df::DataFrame)
+"""
+    write_bedfile
+
+    Writes a dataframe into a format that is acceptable as a bed file, or can at least be used by bedtools
+      i)   Sorts the dataframe - needed for things like tabix indexing
+      ii)  Writes the dataframe to disk - bedfiles don't have headers, and are tab delimited
+      iii) removes the quotes around strings.
+"""
+function write_bedfile(output_path,df::DataFrame;sort=true,seq_id_colname=:seq_id,start_pos_colname=:start_pos)
+    if sort
+        sort!(df,cols=[seq_id_colname,start_pos_colname])
+    end
     writetable( output_path, df, separator='\t', header=false)
     cmd = `sed -i -s 's/\"//g' $output_path` #"
     readall(cmd)
 end
 
 # cross over two dataframes using basic bedtools intersect
-function bedtools_intersect(A::DataFrame, B::DataFrame; negate=false)
+function bedtools_intersect(A::DataFrame, B::DataFrame; negate=false,sort=false)
 
     tmpdir = tempdir()
     tmpa=joinpath( tmpdir, "A")
     tmpb=joinpath( tmpdir, "B")
     tmpc=joinpath( tmpdir, "C")
-    write_bedfile(tmpa,A)
-    write_bedfile(tmpb,B)
+    write_bedfile(tmpa,A,sort=sort)
+    write_bedfile(tmpb,B,sort=sort)
     bedtools_cmd="bedtools"
     bedtools_args = ["intersect","-wo","-a",tmpa, "-b",tmpb]
     if negate
@@ -29,19 +40,40 @@ function bedtools_intersect(A::DataFrame, B::DataFrame; negate=false)
     df=readtable(tmpc,separator='\t',header=false)
 end
 
-function bedtools_intersect(pathA::String, B::DataFrame;negate=false)
-    minusv = ""
-    if negate
-      minusv = "-v"
-    end
+function bedtools_intersect(pathA::String, B::DataFrame;negate=false,sort=false)
+
     tmpdir = tempdir()
     tmpb=joinpath( tmpdir, "B")
     tmpc=joinpath( tmpdir, "C")
-    write_bedfile(tmpb,B)
-    cmd=`bedtools intersect $minusv -wo -a $pathA -b $tmpb` |> tmpc
+    write_bedfile(tmpb,B,sort=sort)
+
+    bedtools_cmd="bedtools"
+    bedtools_args = ["intersect","-wo","-a",pathA, "-b",tmpb]
+    if negate
+       # splices in between 2nd and 1st element with out removing any elements
+       splice!(bedtools_args,2:1,["-v"])
+    end
+    #cmd=`bedtools intersect $minusv -wo -a $pathA -b $tmpb` |> tmpc
+    cmd=`$bedtools_cmd $bedtools_args` |> tmpc
     readall(cmd)
     df=readtable(tmpc,separator='\t',header=false)
 end
+
+function bedtools_intersect(pathA::String, pathB::String;negate=false)
+    tmpdir = tempdir()
+    tmpc=joinpath( tmpdir, "C")
+    bedtools_cmd="bedtools"
+    bedtools_args = ["intersect","-wo","-a",pathA, "-b",pathB]
+    if negate
+       # splices in between 2nd and 1st element with out removing any elements
+       splice!(bedtools_args,2:1,["-v"])
+    end
+    #cmd=`bedtools intersect $minusv -wo -a $pathA -b $tmpb` |> tmpc
+    cmd=`$bedtools_cmd $bedtools_args` |> tmpc
+    readall(cmd)
+    df=readtable(tmpc,separator='\t',header=false)
+end
+
 
 # converts files which have
 # chr1 start strand ... to
