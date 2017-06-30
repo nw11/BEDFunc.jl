e34fea057ab7052fabbcd3731b383a439f590982
nw11 <nicolas.walker@gmail.com>
2017-06-30 20:40:41 +0100

    update test

--- test/test1.jl
+++ test/test1.jl
@@ -1,4 +1,10 @@
 
+using Base.Test
 DIR = dirname(dirname(@__FILE__()))
-path=joinpath(DIR,"src/BEDFunc.jl")
+path=joinpath(DIR,"src/gtf.jl")
 include(path)
+filename = joinpath(DIR,"testdata/gencode.v25.annotation.100.gtf")
+df=parse_gtf_gene_features_to_df(filename, [], ["gene"])
+
+
+@test nrow(df) == 13
